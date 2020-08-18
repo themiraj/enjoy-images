abf3ef3c4f582b180c0436574cb95ffdd633a215
Mohd <mohd@infino.co>
2020-08-18 17:10:34 +0530

    add home

--- src/Component/pages/home.jsx
+++ src/Component/pages/home.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import {Container,Grid,Paper,Box,Button} from '@material-ui/core';
+import axios from 'axios';
 
 class Home extends React.Component {
+    state = {
+        persons: []
+      }
+      componentDidMount() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+          const persons = res.data;
+          this.setState({ persons });
+        })
+      }
     render() {
         return (
             <Grid container spacing={3}>
+
                 <Grid item xs={3}>
-                    <Box color="text.primary" borderRadius={16} borderColor="grey.500">
-                        <Button>Default</Button>
+                    <Box borderColor="primary.main" style={{border: "1px solid"}} borderRadius={12}>
+                        <Button >Default</Button>
                     </Box>
-                </Grid>
-                <Grid item xs={3}>
-                <Paper>xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                <Paper>xs=3</Paper>
-                </Grid>
-                <Grid item xs={3}>
-                <Paper>xs=3</Paper>
                 </Grid>
             </Grid>
         )
